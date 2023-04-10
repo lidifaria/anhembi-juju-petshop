5940142e701d1492dd99cb857de661a18e911876
lidifaria <falecom@lidifaria.com>
2023-04-10 12:21:47 -0300

    2023.04.10 – feat: add cart.ejs

--- controllers/customerCartController.js
+++ controllers/customerCartController.js
@@ -1,2 +1,17 @@
 // Responsável por exibir o carrinho, podendo ter outras funções que trabalhem a lógica do carrinho
 
+const customerCartController = {}
+
+
+// Para armazenar o produto
+add ProductInCart: (req, res) => {
+
+// se a sessão for iniciada, tiver algo e/ou não for undefined, incluir na página de carrinho
+    const product = products.find (p=> p.id == id);
+    if (req.session.cart != undefined){
+        const productSelect = req.session.cart.find(p => parseInt(p.id)==parseInt(id));
+
+    if (productSelect){
+        productSelect.
+    }
+    }
